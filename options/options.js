a498a83ae0b3a61f20f1625cdcac75e77cc83c58
Crowdbotics <team@crowdbotics.com>
2023-08-31 01:07:47 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "soft-surf-43158",
+  url: "https://soft-surf-43158.botics.co",
+  api: "https://soft-surf-43158.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
